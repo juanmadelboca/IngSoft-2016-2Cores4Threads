b0dc1c22678655b39f9eebc78d12bccaf5ae66d6
FrancoRivero <francorivero2012@gmail.com>
2016-06-12 20:28:10 -0300

    agrego test para probar creacion de las preguntas correctamente

---
+++ src/test/java/CreateQuestion.java
@@ -0,0 +1,22 @@
+package main.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreateQuestion {
+	String [] respuestas = {"franco"," mario", "mariano", "juan"};
+     Question q = new Question("¿tu nombre es",respuestas,"franco");
+	@Test
+	public void test() {
+		assertEquals("franco",q.getTrueAnswer());
+		assertNotNull("el objeto es null",new Question("¿tu nombre es",respuestas,"franco"));
+		assertEquals("¿tu nombre es",q.getAnswer());
+		assertEquals("franco",q.getAnswer()[0]);
+		assertEquals("mario",q.getAnswer()[1]);
+		assertEquals("mariano",q.getAnswer()[2]);
+		assertEquals("juan",q.getAnswer()[3]);
+	}
+	
+
+}
