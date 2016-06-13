5ea2d73fefea1870c78f33428091c4357983ffe7
FrancoRivero <francorivero2012@gmail.com>
2016-06-12 23:04:43 -0300

    Agrego test de la clase question

---
+++ src/test/java/CreateQuestionTest.java
@@ -0,0 +1,42 @@
+package test.java;
+
+import static org.junit.Assert.*;
+import main.java.Question;
+
+import org.junit.Test;
+
+public class CreateQuestionTest {
+	String [] respuestas = {"franco"," mario", "mariano", "juan"};
+     Question q = new Question("¿tu nombre es?",respuestas,"franco");
+	@Test
+	public void testCorrecta() {
+		assertEquals("franco",q.getTrueAnswer());
+	}
+	@Test
+	public void testIncorrecta() {
+		assertNotEquals("mario",q.getTrueAnswer());
+	}
+	@Test
+	public void testGetAnswer(){
+		assertEquals("¿tu nombre es?",q.getQuestion());
+	}
+	@Test
+    public void testCreacionDeObjeto(){
+    assertNotNull("el objeto es null",q);	
+    }
+	@Test
+	public void testNullParameters1(){
+		Question q1 = new Question(null,null,null);
+		assertNotNull("el objeto es null",q1.getAnswer());
+	}
+	@Test
+	public void testNullParameters2(){
+		Question q1 = new Question(null,null,null);
+		assertNotNull("el objeto es null",q1.getQuestion());
+	}
+	@Test
+	public void testNullParameters3(){
+		Question q1 = new Question(null,null,null);
+		assertNotNull("el objeto es null",q1.getTrueAnswer());
+	}
+}
